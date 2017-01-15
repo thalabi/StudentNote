5914f5b3071cb0ce947806d4ec7ae1934a3c1b12
Tarif Halabi <tarif.halabi@gmail.com>
2017-01-14 20:55:53 -0500

    Date and time, validation complete.

---
+++ src/app/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+   public static TIMESTAMP_PATTERN = /^\s*(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\s*(\d{1,2})\s*,?\s*(\d{4})\s*(\d{1,2})\s*:?\s*(\d{1,2})\s*(am|pm)\s*$/i;
+   public static MONTHS: string[] = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+}
--- src/app/note-details-form/note-details-form.component.ts
+++ src/app/note-details-form/note-details-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Student } from '../Student';
 import { Note } from '../Note';
 import { StudentService } from '../student.service';
 import { SessionDataService } from '../session-data.service';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { Constants } from '../constants';
 
 @Component({
   selector: 'app-note-details-form',
@@ -19,6 +20,7 @@ export class NoteDetailsFormComponent implements OnInit {
   note: Note;
   crudMode: string;
   datePipe: DatePipe = new DatePipe('en-US');
+  // timestampBlur: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -38,7 +40,8 @@ export class NoteDetailsFormComponent implements OnInit {
     }
 
     this.noteForm = this.formBuilder.group({
-      'timestamp' : [{value: this.datePipe.transform(this.note.timestamp, 'MMM dd, yyyy hh:mm a')  , disabled: this.crudMode == 'Delete'}],
+      'timestamp' : [{value: this.datePipe.transform(this.note.timestamp, 'MMM dd, yyyy hh:mm a'), disabled: this.crudMode == 'Delete'},
+                      dateValidator],
       'text' : [{value: this.note.text, disabled: this.crudMode == 'Delete'}]
     })
 
@@ -48,7 +51,19 @@ export class NoteDetailsFormComponent implements OnInit {
   }
 
   onValueChanged(data?: any) {
-    console.log('onValueChanged')
+/*
+    const timestampControl = this.noteForm.get('timestamp');
+    console.log('onValueChanged(), timestampControl: ', timestampControl);
+    if (timestampControl.valid) {
+      const timestampString = timestampControl.value;
+      const matches = timestampString.match(Constants.TIMESTAMP_PATTERN);
+      console.log('onValueChanged(), matches: ', matches);
+      console.log('onValueChanged(), indexOf: ', Constants.MONTHS.indexOf(matches[1].toLowerCase()));
+      const [year, monthIndex, day, hour, minute] = [matches[3], Constants.MONTHS.indexOf(matches[1].toLowerCase()), matches[2], +matches[4] + (matches[6].toLowerCase() == 'pm' ? 12 : 0), matches[5]];
+      const timestamp = new Date(year, monthIndex, day, hour, minute, 0, 0);
+      console.log('onValueChanged(), timestamp: ', timestamp);
+    }
+*/    
   }
 
   onSubmit() {
@@ -74,4 +89,30 @@ export class NoteDetailsFormComponent implements OnInit {
       this.router.navigate(['noteTable']);
   }
 
+}
+function dateValidator(control: FormControl): {[key: string]: any} {
+  const timestamp = control.value;
+  const patternValid = Constants.TIMESTAMP_PATTERN.test(timestamp);
+  // console.log('dateValidator(), patternValid: ', patternValid);
+  const matches = timestamp.match(Constants.TIMESTAMP_PATTERN);
+  // console.log('dateValidator(), matches: ', matches);
+  let timestampValid = true;
+  if (matches) {
+    let [year, monthIndex, day, hour, minute] = [matches[3], Constants.MONTHS.indexOf(matches[1].toLowerCase()), matches[2], +matches[4] + (matches[6].toLowerCase() == 'pm' ? 12 : 0), matches[5]];
+    // console.log('dateValidator(), time components: ', year, monthIndex, day, hour, minute);
+    if (monthIndex == -1 || day > 31 || hour > 23 || minute > 60) {
+      timestampValid = false;
+    } else if ([1,3,5,7,8,10,12].indexOf(monthIndex+1) == -1 && day == 31) {
+      timestampValid = false;
+    } else if (monthIndex+1 == 2) {
+        if (((year % 4 == 0 && year % 100) || year % 400 == 0) && day == 29) {
+            timestampValid = true;
+        } else if (day > 28) {
+            timestampValid = false;
+        }
+    }
+    console.log('dateValidator(), timestampValid: ', timestampValid);
+  }
+  
+  return matches && timestampValid ? null : {invalidDate: true};
 }
