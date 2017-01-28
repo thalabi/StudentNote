ccb83ad65ca1aa069649fa809af83a6eed42efe7
Tarif Halabi <tarif.halabi@gmail.com>
2017-01-27 22:41:28 -0500

    Removed unused components. 1. note-details-component 2. student-details-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,7 @@ import { Component2Component } from './component2/component2.component';
 import { AppRouting } from './app.routing';
 import { StudentTableComponent } from './student-table/student-table.component'
 import { _404Component } from './404.component';
-import { StudentDetailsComponent } from './student-details/student-details.component';
 import { NoteTableComponent } from './note-table/note-table.component';
-import { NoteDetailsComponent } from './note-details/note-details.component';
 import { StudentDetailsFormComponent } from './student-details-form/student-details-form.component';
 import { NoteDetailsFormComponent } from './note-details-form/note-details-form.component'
 
@@ -24,9 +22,7 @@ import { NoteDetailsFormComponent } from './note-details-form/note-details-form.
     Component2Component,
     StudentTableComponent,
     _404Component,
-    StudentDetailsComponent,
     NoteTableComponent,
-    NoteDetailsComponent,
     StudentDetailsFormComponent,
     NoteDetailsFormComponent
   ],
