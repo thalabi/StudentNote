8a685d4611150b70cd128c60fe66313d5b150a14
Tarif Halabi <tarif.halabi@gmail.com>
2017-02-23 13:02:40 -0500

    Minor cleanup and add some debug code

--- src/app/security/authentication.service.ts
+++ src/app/security/authentication.service.ts
@@ -28,14 +28,19 @@ export class AuthenticationService {
     login(username: string, password: string) {
         return this.http.post(Constants.STUDENT_NOTES_SERVICE_URL+'/Security/authenticate', JSON.stringify({ username: username, password: password }), {headers: this.jsonHeaders})
             .map((response: Response) => {
+                console.log('response: ', response);
                 // login successful if there's a jwt token in the response
                 this.user = response.json();
                 if (this.user && this.user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    //localStorage.setItem('currentUser', JSON.stringify(this.user));
+                    // store user details in SessionDataService
                     this.isAuthenticated = true;
                     this.userSubject.next(this.user);
                     this.sessionDataService.user = this.user;
+                    console.log('JSON.stringify(this.user): ', JSON.stringify(this.user));
+                    console.log('this.user: ', this.user);
+                    for (let authority of this.user.authorities) {
+                        console.log('authority: ', authority);
+                    }
                 } else {
                     //this.userObservable = Observable.from([null]);
                     this.isAuthenticated = false;
