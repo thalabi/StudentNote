4e2cb3d759307f0eda12cffaf686842d58aaca48
Tarif Halabi <tarif.halabi@gmail.com>
2017-03-04 23:08:54 -0500

    Update favicon and content of about page.

--- src/app/StudentNotes.bat
+++ src/app/StudentNotes.bat
@@ -1,10 +1,10 @@
 @echo off
-portqry -q -n localhost -e 8080 || start javaw -jar studentNotes-0.0.1-SNAPSHOT.jar
+portqry -q -n 127.0.0.1 -e 8080 || start javaw -jar studentNotes-2.0.0-SNAPSHOT.jar
 :loop-for-service
 timeout /t 3 >null
-portqry -q -n localhost -e 8080 || goto loop-for-service
-portqry -q -n localhost -e 8081 || start mongoose-free-6.5.exe
+portqry -q -n 127.0.0.1 -e 8080 || goto loop-for-service
+portqry -q -n 127.0.0.1 -e 8081 || start mongoose-free-6.5.exe
 :loop-for-web-server
 timeout /t 1 >null
-portqry -q -n localhost -e 8081 || goto loop-for-web-server
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8081
+portqry -q -n 127.0.0.1 -e 8081 || goto loop-for-web-server
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://127.0.0.1:8081
