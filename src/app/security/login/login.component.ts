12dd7510c591eacd58b2e2046bf4c5dc6856e5d1
Tarif Halabi <tarif.halabi@gmail.com>
2017-03-25 22:36:54 -0400

    Add clear messages on ngOnInit()

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -28,6 +28,8 @@ export class LoginComponent implements OnInit {
  
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+        this.messageService.clear();
   }
 
   login() {
